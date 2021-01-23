16826e58af5e1a367b9df48786558b30c8385377
Be-Man <babene03@gmx.de>
2021-01-23 19:25:24 +0100

    Update men_autogen_cop.hpp

--- men_autogen_cop.hpp
+++ men_autogen_cop.hpp
@@ -2,13 +2,15 @@ class B_soldier_F;
 
 class B_FACTIONPOLICEgamermaniac_Police_Men_Uniform_LvL1_01 : B_soldier_F {
 	author = "Kane Messias";
-	_generalMacro = "B_soldier_F";
-	scope = 2;
+	scope = 1;
 	displayName = "Police Men Uniform LvL1";
+	model = "\A3\Characters_F\Civil\c_poloshirtpants.p3d";
+	hiddenSelections[] = {"camo"};
+	HiddenSelectionsTextures[] = {"\Gamermaniac87_uniforms\textures\Skins\wcpd_recruit.paa"};    //Uniform textures
+	hiddenSelectionsMaterials[]={};
+	
 	side = 1;
 	faction = "FACTION_POLICE_gamermaniac";
-	
-	identityTypes[] = {"Head_Euro"};
 	
 	backpack = "tf_anprc155_coyote";
 	
@@ -20,26 +22,21 @@ class B_FACTIONPOLICEgamermaniac_Police_Men_Uniform_LvL1_01 : B_soldier_F {
 
 	linkedItems[] = {"police_belt_Without_pistol","ItemMap","ItemGPS","The_Programmer_Iphone","ItemCompass"};
 	respawnlinkedItems[] = {"police_belt_Without_pistol","ItemMap","ItemGPS","The_Programmer_Iphone","ItemCompass"};
-	
-	model = "\A3\Characters_F\Civil\c_poloshirtpants.p3d";
-	modelSides[]={3,1};
 
 	nakedUniform="U_BasicBody";
-	uniformClass = "U_FACTIONPOLICE_Police_Uniform_LvL1_men_01";
-	
-	hiddenSelections[] = {"camo"};
-	HiddenSelectionsTextures[] = {"\Gamermaniac87_uniforms\textures\Skins\wcpd_recruit.paa"};    //Uniform textures
 };
 
 class B_FACTIONPOLICEgamermaniac_Police_Men_Uniform_LvL2_01 : B_soldier_F {
 	author = "Kane Messias";
-	_generalMacro = "B_soldier_F";
-	scope = 2;
+	scope = 1;
 	displayName = "Police Men Uniform LvL2";
+	model = "\A3\Characters_F\Civil\c_poloshirtpants.p3d";
+	hiddenSelections[] = {"camo"};
+	HiddenSelectionsTextures[] = {"\Gamermaniac87_uniforms\textures\Skins\wcpd_officer.paa"};    //Uniform textures
+	hiddenSelectionsMaterials[]={};
+	
 	side = 1;
 	faction = "FACTION_POLICE_gamermaniac";
-	
-	identityTypes[] = {"Head_Euro"};
 	
 	backpack = "tf_anprc155_coyote";
 	
@@ -51,26 +48,21 @@ class B_FACTIONPOLICEgamermaniac_Police_Men_Uniform_LvL2_01 : B_soldier_F {
 
 	linkedItems[] = {"police_belt_Without_pistol","ItemMap","ItemGPS","The_Programmer_Iphone","ItemCompass"};
 	respawnlinkedItems[] = {"police_belt_Without_pistol","ItemMap","ItemGPS","The_Programmer_Iphone","ItemCompass"};
-	
-	model = "\A3\Characters_F\Civil\c_poloshirtpants.p3d";
-	modelSides[]={3,1};
 
 	nakedUniform="U_BasicBody";
-	uniformClass = "U_FACTIONPOLICE_Police_Uniform_LvL2_men_01";
-	
-	hiddenSelections[] = {"camo"};
-	HiddenSelectionsTextures[] = {"\Gamermaniac87_uniforms\textures\Skins\wcpd_officer.paa"};    //Uniform textures
 };
 
 class B_FACTIONPOLICEgamermaniac_Police_Men_Uniform_LvL3_01 : B_soldier_F {
 	author = "Kane Messias";
-	_generalMacro = "B_soldier_F";
-	scope = 2;
+	scope = 1;
 	displayName = "Police Men Uniform LvL3";
+	model = "\A3\Characters_F\Civil\c_poloshirtpants.p3d";
+	hiddenSelections[] = {"camo"};
+	HiddenSelectionsTextures[] = {"\Gamermaniac87_uniforms\textures\Skins\wcpd_corporal.paa"};    //Uniform textures
+	hiddenSelectionsMaterials[]={};
+	
 	side = 1;
 	faction = "FACTION_POLICE_gamermaniac";
-	
-	identityTypes[] = {"Head_Euro"};
 	
 	backpack = "tf_anprc155_coyote";
 	
@@ -82,26 +74,21 @@ class B_FACTIONPOLICEgamermaniac_Police_Men_Uniform_LvL3_01 : B_soldier_F {
 
 	linkedItems[] = {"police_belt_Without_pistol","ItemMap","ItemGPS","The_Programmer_Iphone","ItemCompass"};
 	respawnlinkedItems[] = {"police_belt_Without_pistol","ItemMap","ItemGPS","The_Programmer_Iphone","ItemCompass"};
-	
-	model = "\A3\Characters_F\Civil\c_poloshirtpants.p3d";
-	modelSides[]={3,1};
 
 	nakedUniform="U_BasicBody";
-	uniformClass = "U_FACTIONPOLICE_Police_Uniform_LvL3_men_01";
-	
-	hiddenSelections[] = {"camo"};
-	HiddenSelectionsTextures[] = {"\Gamermaniac87_uniforms\textures\Skins\wcpd_corporal.paa"};    //Uniform textures
 };
 
 class B_FACTIONPOLICEgamermaniac_Police_Men_Uniform_LvL4_01 : B_soldier_F {
 	author = "Kane Messias";
-	_generalMacro = "B_soldier_F";
-	scope = 2;
+	scope = 1;
 	displayName = "Police Men Uniform LvL4";
+	model = "\A3\Characters_F\Civil\c_poloshirtpants.p3d";
+	hiddenSelections[] = {"camo"};
+	HiddenSelectionsTextures[] = {"\Gamermaniac87_uniforms\textures\Skins\wcpd_sergeant.paa"};    //Uniform textures
+	hiddenSelectionsMaterials[]={};
+	
 	side = 1;
 	faction = "FACTION_POLICE_gamermaniac";
-	
-	identityTypes[] = {"Head_Euro"};
 	
 	backpack = "tf_anprc155_coyote";
 	
@@ -113,26 +100,21 @@ class B_FACTIONPOLICEgamermaniac_Police_Men_Uniform_LvL4_01 : B_soldier_F {
 
 	linkedItems[] = {"police_belt_Without_pistol","ItemMap","ItemGPS","The_Programmer_Iphone","ItemCompass"};
 	respawnlinkedItems[] = {"police_belt_Without_pistol","ItemMap","ItemGPS","The_Programmer_Iphone","ItemCompass"};
-	
-	model = "\A3\Characters_F\Civil\c_poloshirtpants.p3d";
-	modelSides[]={3,1};
 
 	nakedUniform="U_BasicBody";
-	uniformClass = "U_FACTIONPOLICE_Police_Uniform_LvL4_men_01";
-	
-	hiddenSelections[] = {"camo"};
-	HiddenSelectionsTextures[] = {"\Gamermaniac87_uniforms\textures\Skins\wcpd_sergeant.paa"};    //Uniform textures
 };
 
 class B_FACTIONPOLICEgamermaniac_Police_Men_Uniform_LvL5_01 : B_soldier_F {
 	author = "Kane Messias";
-	_generalMacro = "B_soldier_F";
-	scope = 2;
+	scope = 1;
 	displayName = "Police Men Uniform LvL5";
+	model = "\A3\Characters_F\Civil\c_poloshirtpants.p3d";
+	hiddenSelections[] = {"camo"};
+	HiddenSelectionsTextures[] = {"\Gamermaniac87_uniforms\textures\Skins\wcpd_deputy_chief.paa"};    //Uniform textures
+	hiddenSelectionsMaterials[]={};
+	
 	side = 1;
 	faction = "FACTION_POLICE_gamermaniac";
-	
-	identityTypes[] = {"Head_Euro"};
 	
 	backpack = "tf_anprc155_coyote";
 	
@@ -144,26 +126,21 @@ class B_FACTIONPOLICEgamermaniac_Police_Men_Uniform_LvL5_01 : B_soldier_F {
 
 	linkedItems[] = {"police_belt_Without_pistol","ItemMap","ItemGPS","The_Programmer_Iphone","ItemCompass"};
 	respawnlinkedItems[] = {"police_belt_Without_pistol","ItemMap","ItemGPS","The_Programmer_Iphone","ItemCompass"};
-	
-	model = "\A3\Characters_F\Civil\c_poloshirtpants.p3d";
-	modelSides[]={3,1};
 
 	nakedUniform="U_BasicBody";
-	uniformClass = "U_FACTIONPOLICE_Police_Uniform_LvL5_men_01";
-	
-	hiddenSelections[] = {"camo"};
-	HiddenSelectionsTextures[] = {"\Gamermaniac87_uniforms\textures\Skins\wcpd_deputy_chief.paa"};    //Uniform textures
 };
 
 class B_FACTIONPOLICEgamermaniac_Police_Men_Uniform_LvL6_01 : B_soldier_F {
 	author = "Kane Messias";
-	_generalMacro = "B_soldier_F";
-	scope = 2;
+	scope = 1;
 	displayName = "Police Men Uniform LvL6";
+	model = "\A3\Characters_F\Civil\c_poloshirtpants.p3d";
+	hiddenSelections[] = {"camo"};
+	HiddenSelectionsTextures[] = {"\Gamermaniac87_uniforms\textures\Skins\wcpd_chief_of_department.paa"};    //Uniform textures
+	hiddenSelectionsMaterials[]={};
+	
 	side = 1;
 	faction = "FACTION_POLICE_gamermaniac";
-	
-	identityTypes[] = {"Head_Euro"};
 	
 	backpack = "tf_anprc155_coyote";
 	
@@ -175,13 +152,6 @@ class B_FACTIONPOLICEgamermaniac_Police_Men_Uniform_LvL6_01 : B_soldier_F {
 
 	linkedItems[] = {"police_belt_Without_pistol","ItemMap","ItemGPS","The_Programmer_Iphone","ItemCompass"};
 	respawnlinkedItems[] = {"police_belt_Without_pistol","ItemMap","ItemGPS","The_Programmer_Iphone","ItemCompass"};
-	
-	model = "\A3\Characters_F\Civil\c_poloshirtpants.p3d";
-	modelSides[]={3,1};
 
 	nakedUniform="U_BasicBody";
-	uniformClass = "U_FACTIONPOLICE_Police_Uniform_LvL6_men_01";
-	
-	hiddenSelections[] = {"camo"};
-	HiddenSelectionsTextures[] = {"\Gamermaniac87_uniforms\textures\Skins\wcpd_chief_of_department.paa"};    //Uniform textures
 };
