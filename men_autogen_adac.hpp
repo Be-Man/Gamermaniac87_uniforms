34deda083db9dfba68161ed6c9b55f3f1e04b788
Be-Man <babene03@gmx.de>
2021-01-23 19:24:08 +0100

    Update men_autogen_adac.hpp

--- men_autogen_adac.hpp
+++ men_autogen_adac.hpp
@@ -2,14 +2,16 @@ class C_man_w_worker_F;
 
 class I_FACTIONADACgamermaniac_ADAC_Uniform_men_01 : C_man_w_worker_F {
 	author = "Kane Messias";
-	_generalMacro = "C_man_w_worker_F";
 	scope = 2;
 	displayName = "ADAC Men Uniform";
+	model="\A3\characters_F\common\coveralls";
+	hiddenSelections[] = {"camo"};
+	HiddenSelectionsTextures[] = {"Gamermaniac87_uniforms\textures\Skins\adac_uniform.paa"};    //Uniform textures
+	hiddenSelectionsMaterials[] = {};
+	
 	side = 2;
 	faction = "FACTION_ADAC_gamermaniac";
-
-	identityTypes[] = {"Head_Euro"};
-
+	
 	backpack = "tf_anprc155_coyote";
 
 	weapons[] = {};
@@ -20,14 +22,6 @@ class I_FACTIONADACgamermaniac_ADAC_Uniform_men_01 : C_man_w_worker_F {
 
 	linkedItems[] = {"EF_BLT_MEMS","ItemMap","ItemGPS","The_Programmer_Iphone","ItemCompass","ItemWatch"};
 	respawnlinkedItems[] = {"EF_BLT_MEMS","ItemMap","ItemGPS","The_Programmer_Iphone","ItemCompass","ItemWatch"};
-	
-	model="\A3\characters_F\common\coveralls";
-	modelSides[]={3};
 
 	nakedUniform="U_BasicBody";
-	uniformClass = "U_FACTIONADAC_adac_Uniform_men_01";
-
-	hiddenSelections[] = {"camo"};
-	HiddenSelectionsTextures[] = {"Gamermaniac87_uniforms\textures\Skins\adac_uniform.paa"};    //Uniform textures
-
 };
