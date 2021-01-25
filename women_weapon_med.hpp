d715f6ab49a7438abbb469c878cc506d0377ca94
Be-Man <babene03@gmx.de>
2021-01-25 17:36:45 +0100

    Add Testing Protections

--- women_weapon_med.hpp
+++ women_weapon_med.hpp
@@ -17,5 +17,57 @@ class U_FACTIONMEDIC_Medic_Uniform_women_01: Uniform_Base {
 		uniformClass="O_FACTIONMEDICgamermaniac_Medical_Woman_Uniform_01";
 		containerClass="Supply40";
 		mass=10;
+		
+		//more info at: https://community.bistudio.com/wiki/Arma_3_Soldier_Protection
+		class HitpointsProtectionInfo {
+			class Neck
+			{
+				hitpointName	= "HitNeck";	// reference to the hit point class defined in the man base class
+				armor			= 8;			// addition to armor of referenced hitpoint
+				passThrough		= 0.5;			// multiplier of base passThrough defined in referenced hitpoint
+			};
+			class Arms
+			{
+				hitpointName	= "HitArms";
+				armor			= 8;
+				passThrough		= 0.5;
+			};
+			class Chest
+			{
+				hitpointName	= "HitChest";
+				armor			= 8;
+				passThrough		= 0.1;
+			};
+			class Diaphragm
+			{
+				hitpointName	= "HitDiaphragm";
+				armor			= 8;
+				passThrough		= 0.1;
+			};
+			class Abdomen
+			{
+				hitpointName	= "HitAbdomen";
+				armor			= 8;
+				passThrough		= 0.1;
+			};
+			class Body
+			{
+				hitpointName	= "HitBody";
+				armor			= 8;
+				passThrough		= 0.1;
+			};
+			class pelvis
+			{
+				hitpointName	= "HitPelvis";
+				armor			= 8;
+				passThrough		= 0.1;
+			};
+			class legs
+			{
+				hitpointName	= "HitLegs";
+				armor			= 8;
+				passThrough		= 0.1;
+			};
+		};
 	};
 };
